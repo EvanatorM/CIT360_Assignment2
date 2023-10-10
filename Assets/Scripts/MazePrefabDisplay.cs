69581511071a5581bd8aad63297a0ec4d04caf48
WSAL Evan <evan@millers6.com>
2023-10-10 17:29:57 -0400

    Restructured and started tilemap test

---
+++ Assets/Scripts/MazePrefabDisplay.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazePrefabDisplay : MonoBehaviour
+{
+    [Header("Maze Generation")]
+    [SerializeField] int mazeSizeX, mazeSizeY;
+
+    [Header("Maze Display")]
+    [SerializeField] PhysicalMazeNode physicalMazeNodePrefab;
+    [SerializeField] float mazeNodeSize;
+
+    Maze maze;
+
+    void Start()
+    {
+        maze = new Maze(mazeSizeX, mazeSizeY);
+
+        DisplayMazePrefabs();
+    }
+
+    void DisplayMazePrefabs()
+    {
+        for (int x = 0; x < mazeSizeX; x++)
+        {
+            for (int y = 0; y < mazeSizeY; y++)
+            {
+                PhysicalMazeNode mazeNode = Instantiate(physicalMazeNodePrefab, new Vector3(x * mazeNodeSize, y * mazeNodeSize, 0), Quaternion.identity);
+                mazeNode.InitPhysicalMadeNode(maze.nodes[x, y], mazeNodeSize);
+            }
+        }
+
+        Camera.main.transform.position = new Vector3((mazeSizeX / 2.0f - 0.5f) * mazeNodeSize, (mazeSizeY / 2.0f - 0.5f) * mazeNodeSize, -10);
+        Camera.main.orthographicSize = mazeSizeY * mazeNodeSize / 2f + 1;
+    }
+}
--- Assets/Scripts/MazeTilemapDisplay.cs
+++ Assets/Scripts/MazeTilemapDisplay.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class MazeDisplay : MonoBehaviour
+public class MazeTilemapDisplay : MonoBehaviour
 {
     [Header("Maze Generation")]
     [SerializeField] int mazeSizeX, mazeSizeY;
 
     [Header("Maze Display")]
-    [SerializeField] PhysicalMazeNode physicalMazeNodePrefab;
+    [SerializeField] Tilemap tilemap;
+    [SerializeField] Tile blankTile, wallTile;
     [SerializeField] float mazeNodeSize;
 
     Maze maze;
@@ -17,19 +19,12 @@ public class MazeDisplay : MonoBehaviour
     {
         maze = new Maze(mazeSizeX, mazeSizeY);
 
-        DisplayMazePrefabs();
+        DisplayMazeTilemaps();
     }
 
-    void DisplayMazePrefabs()
+    void DisplayMazeTilemaps()
     {
-        for (int x = 0; x < mazeSizeX; x++)
-        {
-            for (int y = 0; y < mazeSizeY; y++)
-            {
-                PhysicalMazeNode mazeNode = Instantiate(physicalMazeNodePrefab, new Vector3(x * mazeNodeSize, y * mazeNodeSize, 0), Quaternion.identity);
-                mazeNode.InitPhysicalMadeNode(maze.nodes[x, y], mazeNodeSize);
-            }
-        }
+
 
         Camera.main.transform.position = new Vector3((mazeSizeX / 2.0f - 0.5f) * mazeNodeSize, (mazeSizeY / 2.0f - 0.5f) * mazeNodeSize, -10);
         Camera.main.orthographicSize = mazeSizeY * mazeNodeSize / 2f + 1;
