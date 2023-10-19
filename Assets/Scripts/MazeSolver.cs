90d70f2f0ff76e75446f5691db9505b536ef1025
WSAL Evan <evan@millers6.com>
2023-10-19 19:02:27 -0400

    Maze solver

--- Assets/Scripts/MazeSolver.cs
+++ Assets/Scripts/MazeSolver.cs
@@ -1,33 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MazeSolver : MonoBehaviour
 {
-    MazeSolvePoint[,] maze = new MazeSolvePoint[10, 10];
-
-    public void SolveMaze(Maze mazeToSolve)
+    public class MazeSolvePoint
     {
-        for (int y = 0; y < mazeToSolve.mazeSizeY; y++)
-        {
-            for (int x = 0; x < mazeToSolve.mazeSizeX; x++)
-            {
-                if (wall)
-                    maze[x, y] = new MazeSolvePoint();
-                else
-                    maze[x, y] = new MazeSolvePoint();
+        public int x, y, data;
+        public int type;
+        public bool visited;
 
+        public MazeSolvePoint(int x, int y, int type)
+        {
+            this.x = x;
+            this.y = y;
+            data = 0;
+            this.type = type;
+            visited = false;
+        }
+
+        public bool isWalkable()
+        {
+            return type != -1;
+        }
+    }
+
+    [Header("UI")]
+    [SerializeField] Tilemap tilemap;
+    [SerializeField] Tile pathTile, startTile, endTile;
+    
+    // Maze variables
+    MazeSolvePoint[,] maze;
+    MazeSolvePoint start;
+    MazeSolvePoint end;
+    int sizeX, sizeY;
+
+    public void SolveMaze(int[,] mazeToSolve)
+    {
+        sizeX = mazeToSolve.GetLength(0);
+        sizeY = mazeToSolve.GetLength(1);
+
+        maze = new MazeSolvePoint[sizeX, sizeY];
+
+        // Generate maze solve point array
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                maze[x, y] = new MazeSolvePoint(x, y, mazeToSolve[x, y]);
             }
         }
 
         // Pick start and end points
-        MazeSolvePoint start = maze[rnd1][rnd2];
-        MazeSolvePoint end = maze[rnd3][rnd4];
+        start = GetRandomPoint();
+        do
+        {
+            end = GetRandomPoint();
+        }
+        while (end == start);
+
+        DFS();
+        FindPath();
+        DisplayMazePath();
     }
 
-    public void dfs()
+    MazeSolvePoint GetRandomPoint()
     {
-        Stack<MazeSolvePoint> s = new Stack<MazeSolvePoint>(row * col);
+        MazeSolvePoint point;
+        do
+        {
+            point = maze[Random.Range(0, sizeX), Random.Range(0, sizeY)];
+        }
+        while (!point.isWalkable());
+
+        return point;
+    }
+
+    public void DFS()
+    {
+        Stack<MazeSolvePoint> s = new Stack<MazeSolvePoint>(sizeX * sizeY);
         s.Push(start);
         int distance = 0;
 
@@ -44,35 +96,126 @@ public class MazeSolver : MonoBehaviour
             if (curr == end)
                 return;
 
-            if (curr.row > 1) // Up
+            if (curr.x > 1) // Up
             {
-                MazeSolvePoint n = maze[curr.row - 1, curr.col];
+                MazeSolvePoint n = maze[curr.x - 1, curr.y];
                 if (n.isWalkable() && !n.visited)
                         s.Push(n);
             }
-            if (curr.col < col) // Right
+            if (curr.y < sizeY) // Right
             {
-                MazeSolvePoint n = maze[curr.row, curr.col + 1];
+                MazeSolvePoint n = maze[curr.x, curr.y + 1];
                 if (n.isWalkable() && !n.visited)
                     s.Push(n);
             }
-            if (curr.row < row) // Down
+            if (curr.x < sizeX) // Down
             {
-                MazeSolvePoint n = maze[curr.row + 1, curr.col];
+                MazeSolvePoint n = maze[curr.x + 1, curr.y];
                 if (n.isWalkable() && !n.visited)
                         s.Push(n);
             }
-            if (curr.col > 1) // Left
+            if (curr.y > 1) // Left
             {
-                MazeSolvePoint n = maze[curr.row, curr.col - 1];
+                MazeSolvePoint n = maze[curr.x, curr.y - 1];
                 if (n.isWalkable() && !n.visited)
                     s.Push(n);
             }
         }
     }
 
-    void bfs()
+    public void FindPath()
     {
+        MazeSolvePoint currentPoint = end;
 
+        while (currentPoint != start)
+        {
+            MazeSolvePoint[] nearPoints = new MazeSolvePoint[4];
+            nearPoints[0] = maze[currentPoint.x, currentPoint.y + 1];
+            nearPoints[1] = maze[currentPoint.x + 1, currentPoint.y];
+            nearPoints[2] = maze[currentPoint.x, currentPoint.y - 1];
+            nearPoints[3] = maze[currentPoint.x - 1, currentPoint.y];
+
+            int minData = int.MaxValue;
+            MazeSolvePoint nextPoint = null;
+            for (int i = 0; i < nearPoints.Length; i++)
+            {
+                if (nearPoints[i] == start)
+                    return;
+
+                if (nearPoints[i].data < minData && nearPoints[i].data > 0)
+                {
+                    minData = nearPoints[i].data;
+                    nextPoint = nearPoints[i];
+                }
+            }
+
+            if (nextPoint == null)
+                return;
+
+            nextPoint.type = 1;
+            currentPoint = nextPoint;
+        }
+    }
+
+    void BFS()
+    {
+        Queue<MazeSolvePoint> s = new Queue<MazeSolvePoint>(sizeX * sizeY);
+        s.Enqueue(start);
+        int distance = 0;
+
+        while (s.Count > 0)
+        {
+            MazeSolvePoint curr = s.Dequeue();
+
+            if (curr.visited)
+                continue;
+
+            curr.visited = true;
+            curr.data = distance++;
+
+            if (curr == end)
+                return;
+
+            if (curr.x > 1) // Up
+            {
+                MazeSolvePoint n = maze[curr.x - 1, curr.y];
+                if (n.isWalkable() && !n.visited)
+                    s.Enqueue(n);
+            }
+            if (curr.y < sizeY) // Right
+            {
+                MazeSolvePoint n = maze[curr.x, curr.y + 1];
+                if (n.isWalkable() && !n.visited)
+                    s.Enqueue(n);
+            }
+            if (curr.x < sizeX) // Down
+            {
+                MazeSolvePoint n = maze[curr.x + 1, curr.y];
+                if (n.isWalkable() && !n.visited)
+                    s.Enqueue(n);
+            }
+            if (curr.y > 1) // Left
+            {
+                MazeSolvePoint n = maze[curr.x, curr.y - 1];
+                if (n.isWalkable() && !n.visited)
+                    s.Enqueue(n);
+            }
+        }
+    }
+
+    void DisplayMazePath()
+    {
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                if (start == maze[x, y])
+                    tilemap.SetTile(new Vector3Int(x, y, 0), startTile);
+                else if (end == maze[x, y])
+                    tilemap.SetTile(new Vector3Int(x, y, 0), endTile);
+                else if (maze[x, y].type == 1)
+                    tilemap.SetTile(new Vector3Int(x, y, 0), pathTile);
+            }
+        }
     }
 }
--- Assets/Scripts/MazeTilemapDisplay.cs
+++ Assets/Scripts/MazeTilemapDisplay.cs
@@ -11,22 +11,93 @@ public class MazeTilemapDisplay : MonoBehaviour
     [Header("Maze Display")]
     [SerializeField] Tilemap tilemap;
     [SerializeField] Tile blankTile, wallTile;
-    [SerializeField] float mazeNodeSize;
+    [SerializeField] int mazeNodeSize;
 
     Maze maze;
+    MazeSolver ms;
 
     void Start()
     {
         maze = new Maze(mazeSizeX, mazeSizeY);
+        ms = FindObjectOfType<MazeSolver>();
 
         DisplayMazeTilemaps();
     }
 
     void DisplayMazeTilemaps()
     {
+        int[,] intArray = ConvertNodesToInt(mazeSizeX, mazeSizeY, mazeNodeSize, maze.nodes);
+        for (int x = 0; x < intArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < intArray.GetLength(1); y++)
+            {
+                if (intArray[x, y] == -1)
+                    tilemap.SetTile(new Vector3Int(x, y, 0), wallTile);
+                else
+                    tilemap.SetTile(new Vector3Int(x, y, 0), blankTile);
+            }
+        }
 
+        float mazeWorldSizeX = mazeSizeX * (mazeNodeSize + 1) + 1;
+        float mazeWorldSizeY = mazeSizeY * (mazeNodeSize + 1) + 1;
+        Camera.main.transform.position = new Vector3(mazeWorldSizeX / 2.0f, mazeWorldSizeY / 2.0f, -10);
+        Camera.main.orthographicSize = mazeWorldSizeY * .5f + 1;
 
-        Camera.main.transform.position = new Vector3((mazeSizeX / 2.0f - 0.5f) * mazeNodeSize, (mazeSizeY / 2.0f - 0.5f) * mazeNodeSize, -10);
-        Camera.main.orthographicSize = mazeSizeY * mazeNodeSize / 2f + 1;
+        // Solve maze
+        ms.SolveMaze(intArray);
+    }
+
+    public int[,] ConvertNodesToInt(int nodeX, int nodeY, int nodeSize, Maze.MazeNode[,] nodes)
+    {
+        int sizeX = nodeX * (nodeSize + 1) + 1;
+        int sizeY = nodeY * (nodeSize + 1) + 1;
+
+        int[,] maze = new int[sizeX, sizeY];
+
+        for (int ny = 0; ny < nodeY; ny++)
+        {
+            for (int nx = 0; nx < nodeX; nx++)
+            {
+                int ax = nx * (nodeSize + 1);
+                int ay = ny * (nodeSize + 1);
+
+                for (int dy = ay; dy < ay + nodeSize + 2; dy++)
+                {
+                    for (int dx = ax; dx < ax + nodeSize + 2; dx++)
+                    {
+                        if (dx == ax) // Left edge
+                        {
+                            // Check for corners
+                            if (dy == ay || dy == ay + nodeSize + 1)
+                                maze[dx, dy] = -1;
+
+                            if (nodes[nx, ny].westWall)
+                                maze[dx, dy] = -1;
+                        }
+                        else if (dx == ax + nodeSize + 1) // Right edge
+                        {
+                            // Check for corners
+                            if (dy == ay || dy == ay + nodeSize + 1)
+                                maze[dx, dy] = -1;
+
+                            if (nodes[nx, ny].eastWall)
+                                maze[dx, dy] = -1;
+                        }
+                        else if (dy == ay) // Bottom edge
+                        {
+                            if (nodes[nx, ny].southWall)
+                                maze[dx, dy] = -1;
+                        }
+                        else if (dy == ay + nodeSize + 1) // Top edge
+                        {
+                            if (nodes[nx, ny].northWall)
+                                maze[dx, dy] = -1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return maze;
     }
 }
